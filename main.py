39622a1dcbd2f36da6ac8f3752d5d9c480d6c208
dylan <dylan.schertzer@outlooK.fr>
2022-09-14 20:51:44 +0200

    fix blue

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from random import choices
 
 point = 0,10,20,30,40
 for i in range(100):
-    bleu = (choices(point , weights= (50,0,5,10,35)))#Fast Overarm 
-    print(f'vous avez gagner {bleu} point')
+    test = (choices(point , weights= (50,0,5,10,35)))#Fast Overarm 
+    print(f'vous avez gagner {test} point')
 
 
 
