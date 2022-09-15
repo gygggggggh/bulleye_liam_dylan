019a43eb658a930960038e69863c910a3acafa5b
dylan <84039229+gygggggggh@users.noreply.github.com>
2022-09-15 11:11:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import choices
 
 point = 0,10,20,30,40
 for i in range(100):
-    test = (choices(point , weights= (50,0,5,10,35)))#Fast Overarm 
+    test = choices(point , weights= (50,0,5,10,35))#Fast Overarm 
     print(f'vous avez gagner {test} point')
 
 
