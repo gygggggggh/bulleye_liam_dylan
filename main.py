c3b0c96a412b1837164e3191bd354fb696699b42
dylan <dylan.schertzer@outlook.fr>
2022-10-05 21:08:39 +0200

    GOODF FINA LCOMMIT

--- main.py
+++ main.py
@@ -1,64 +1,7 @@
 # mini projet
-from random import randint
-from unicodedata import name
-from webbrowser import get
+import random
 
-def Fast_Overarm():
-    """resumé
-    choisi une varible aleatoire et la renvoie
 
-    Returns:
-        int: un entier representen les points 
-    """
-    random = randint(0,100)
-    if random <= 50:
-        return 0
-    if  51 <= random <= 55:
-        return 20
-    if 56 <= random <= 65:
-       return 30
-    if 66 <= random <= 100:
-        return 40
-    
-
-def Controlled_overarm() :
-    """resumé
-    choisi une varible aleatoire et la renvoie
-
-    Returns:
-        int: un entier representen les points 
-    """
-    random = randint(0,101)
-    if random == 0:
-        return 0 
-    if 1 <= random <= 42:
-        return 10
-    if 43 <= random <= 77:
-        return 20
-    if 78 <= random <= 100:
-        return 30
-    if random == 101 :
-        return 40
-
-def Underarm():
-    """resumé
-    choisi une varible aleatoire et la renvoie
-
-    Returns:
-        int: un entier representen les points 
-    """
-    random = randint(0,100)
-    if random <= 5:
-       return 0
-    if 6 <= random <= 46:
-       return 10
-    if 47 <= random <= 77:
-        return 20
-    if 78 <= random <= 98:
-        return 30
-    if random >= 99:
-        return 40
-    
 
 def couleur(r, g, b, texte):
     """resumée 
@@ -91,16 +34,41 @@ def mode_de_tire():
             
             demande = input(couleur(0,100,0,"==>"))
         demande = int(demande)
-          
         if demande == 1:
-                score = Fast_Overarm()
-                break
+            random_tir = random.random()
+            if random_tir <= 0.5:
+                return 0
+            if  0.51 <= random_tir <= 0.55:
+                return 20
+            if 0.56 <= random_tir <= 0.65:
+                return 30
+            if 0.66 <= random_tir <= 1:
+                return 40
         if demande == 2:
-                score = Controlled_overarm()
-                break
+            random_tir = random.random()
+            if random_tir < 0.1:
+                score = 40
+            if 0.1 < random_tir <= 0.22:
+                score = 30
+            if 0.22 < random_tir <= 0.56:
+                score = 20
+            if 0.56 < random_tir <= 0.99:
+                score = 10
+            if random_tir  > 0.99:
+                score = 0
+                             
         if demande == 3:
-            score = Underarm()
-            break
+            random_tir = random.random()
+            if random_tir <= 0.05:
+                score = 0
+            if 0.05 < random_tir <= 0.205:
+                score = 30
+            if 0.205 < random_tir <= 0.555:
+                score = 20
+            if 0.555 < random_tir <= 0.985:
+                score = 10
+            if random_tir > 0.985:
+                return 40
         else:
             print(couleur(255,0,0,"Nombre invalide : minimun 1 maximun 3 \n"))
             print(couleur(135,206,235,"quel mode voulez vous ?\n \n Fast_Overarm [1] \n Controlled_overarm [2] \n Underarm[3]"))
@@ -123,7 +91,7 @@ def enter_jeu():
         for i in range(nb_joueur):
             nom = input(couleur(0,125,0,f"nom du joueur {i+1}\n==>"))
             if nom == "":
-                nom = "joueur " + str(i+1)
+                nom = "joueur" + str(i+1)
                 score_board[nom] = 0
                 
             if  nom in score_board.keys():
